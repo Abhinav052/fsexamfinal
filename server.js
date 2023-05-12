5c9017ec29fc311d4b0042552474786d8c59e060
Abhinav052 <abhiabhinav535@gmail.com>
2023-05-12 16:52:52 +0530

    aslas added

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express=require('express');
-const app=express();
+const express = require('express');
+const app = express();
 require('dotenv').config();
-const port=process.env.PORT || 3000;
-const mongo=require('mongoose');
+const port = process.env.PORT || 3000;
+const mongo = require('mongoose');
 var session = require('express-session')
 const auth = require('./routes/auth');
-const twitter=require('./routes/tweet');
+const twitter = require('./routes/tweet');
 
 // Connect to MongoDB
-mongo.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(function(){
+mongo.connect(process.env.MONGODB_URI_CLOUD, { useNewUrlParser: true, useUnifiedTopology: true }).then(function () {
     console.log('Connected to MongoDB');
-}).catch(function(err){
+}).catch(function (err) {
     console.log(err);
 });
 app.use(session({
@@ -18,11 +18,11 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }))
+}))
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
@@ -32,8 +32,8 @@ app.use(twitter);
 
 
 
-app.listen(port,err=>{
-    if(err) throw err;
-    console.log('Server is running on port: '+port);
+app.listen(port, err => {
+    if (err) throw err;
+    console.log('Server is running on port: ' + port);
 });
 
